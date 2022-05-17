047c5d1445370208a615d67c98c7f8d2dd0ccadb
RudhyDev <rudhy.pereira@gmail.com>
2022-05-16 22:47:41 -0300

    refactor: :recycle: clean the index.js by segregating some code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import http from "node:http";
+import handler from "./handler.js";
 
 const PORT = process.env.PORT || 4444;
 
 const server = http
-	.createServer((req, res) => {
-		res.end("こんにちは世界");
-	})
+	.createServer(handler)
 	.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 export { server };
